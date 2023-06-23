492786fea779b4addad1636eb4902664ced3e068
Zharfan0 <zharfanwidyadana@gmail.com>
2023-06-23 15:28:27 +0700

    iterasi dari awal array hingga REAR

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -72,11 +72,18 @@ public:
 
 		cout << "\nElement in the queues are...\n";
 
-		//jika FRONT_position <= REAR_positionm iterasi dari FRONT hingga REAR
+		//jika FRONT_position <= REAR_position iterasi dari FRONT hingga REAR
 		if (FRONT_position <= REAR_position) {
 			while (FRONT_position <= REAR_position) {
 				cout << queue_array[FRONT_position] << "   ";
 				FRONT_position++;
+			}
+
+			FRONT_position = 0;
+
+			//iterasi dari awal array hingga REAR
+			while (FRONT_position <= REAR_position){
+				cout << queue_array[FRONT_position] << "   ";
 			}
 		}
 	}
